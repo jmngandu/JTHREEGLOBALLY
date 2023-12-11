c2453128b0cec0fbab7db0369b2e275bd1b152c5
jmngandu <jamesmngandu@gmail.com>
2023-12-11 15:55:36 +0300

    added

--- home/main.js
+++ home/main.js
@@ -1,3 +1,6 @@
-const btn = document.querySelector("#btn").addEventListener("click", ()=>{
+const btn = document.querySelector("#main-btn").addEventListener("click", ()=>{
+    document.body.style.backgroundColor = 'dodgerblue';
     alert("please wait the system is under construction")
+    document.body.style.backgroundColor = 'grey';
+
 });
