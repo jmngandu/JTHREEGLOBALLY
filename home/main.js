f888045dae97f898cc5ecd70ac4644760029813c
jmngandu <jamesmngandu@gmail.com>
2023-12-14 06:13:49 +0300

    commit

--- home/main.js
+++ home/main.js
@@ -2,5 +2,4 @@ const btn = document.querySelector("#main-btn").addEventListener("click", ()=>{
     document.body.style.backgroundColor = 'dodgerblue';
     alert("please wait the system is under construction")
     document.body.style.backgroundColor = 'grey';
-
 });
