15833316ea53ef7c94040ea5a51e50b6f24a44cd
jmngandu <jamesmngandu@gmail.com>
2024-01-17 16:49:25 +0300

    commit

--- home/main.js
+++ home/main.js
@@ -1,14 +1,5 @@
-// const btn = document.querySelector("#main-btn").addEventListener("click", ()=>{
-//     document.body.style.backgroundColor = 'dodgerblue';
-//     alert("please wait the system is under construction")
-//     document.body.style.backgroundColor = 'grey';
-// });
-let count = 0
-function prnt(){
-    alert("am done")
-}
-let countEl = document.getElementById("count-el")
-function countBtn(){
-   countEl.textContent = `count: ${count+=1}`
-    prnt()
-}
+const btn = document.querySelector("#main-btn").addEventListener("click", ()=>{
+    document.body.style.backgroundColor = 'dodgerblue';
+    alert("please wait the system is under construction")
+    document.body.style.backgroundColor = 'grey';
+});
