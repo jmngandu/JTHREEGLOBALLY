a420d1c786f9b3dfc55fa51d421d9f98e695c132
jmngandu <jamesmngandu@gmail.com>
2024-01-18 18:07:09 +0300

    commit

--- home/main.js
+++ home/main.js
@@ -3,3 +3,9 @@ const btn = document.querySelector("#main-btn").addEventListener("click", ()=>{
     alert("please wait the system is under construction")
     document.body.style.backgroundColor = 'grey';
 });
+
+const helpTag = document.getElementById("help-tag")
+
+helpTag.addEventListener("dblclick",()=>{
+    helpTag.innerHTML = "<ul> Help page not Ready</ul>"
+})
