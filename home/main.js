fc34dfac5c0cd9c6be3dd763deb7c1f2076ddb25
jmngandu <jamesmngandu@gmail.com>
2024-02-02 16:53:50 +0300

    commit

--- home/main.js
+++ home/main.js
@@ -1,23 +1,21 @@
 const buyProducts = document.getElementById("buy-products")
-const btn = document.querySelector("#main-btn").addEventListener("click", ()=>{
-    // document.body.style.backgroundColor = 'dodgerblue';
+const btn = document.querySelector("#main-btn").addEventListener("click", () => { // document.body.style.backgroundColor = 'dodgerblue';
     buyProducts.innerHTML = "<button onclick='ourProducts()'>EXPLORE PRODUCTS</button>"
 });
 
-function ourProducts(){
+function ourProducts() {
     alert("please wait ...")
     buyProducts.innerHTML = "<button onclick='ourProducts()'>THANKS FOR SHOPPING</button>"
 }
 let helpTag = document.getElementById("help-tag")
-helpTag.addEventListener("dblclick",()=>{
+helpTag.addEventListener("dblclick", () => {
     document.body.style.backgroundColor = 'grey';
     helpTag.innerText = "DARK THEME"
 })
-helpTag.addEventListener("click",()=>{
+helpTag.addEventListener("click", () => {
     document.body.style.backgroundColor = 'white';
     helpTag.innerText = "LIGHT THEME"
 })
-
 
 
 let faqs = document.getElementById("faqs")
