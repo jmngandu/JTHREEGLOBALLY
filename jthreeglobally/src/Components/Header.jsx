78a7e5390d738a52740be3897ea4aea4452aeb61
jmngandu <jamesmngandu@gmail.com>
2024-03-27 14:22:01 +0300

    added header

---
+++ jthreeglobally/src/Components/Header.jsx
@@ -0,0 +1,6 @@
+
+export const Header = ()=>{
+    return(
+        <h1>header section</h1>
+    )
+}
--- jthreeglobally/src/pages/Home.jsx
+++ jthreeglobally/src/pages/Home.jsx
@@ -1,7 +1,9 @@
-
+import React from "react"
+import { Header } from "../Components/Header"
 export const Home =()=>{
     return(
         <>
+        <Header/>
         <h1>Home</h1>
         </>
     )
