0a0bec24a930ae5ec33c169a883e7745fc8d66cf
jmngandu <jamesmngandu@gmail.com>
2024-03-27 14:50:45 +0300

    created dashboard

--- jthreeglobally/src/Components/Header.jsx
+++ jthreeglobally/src/Components/Header.jsx
@@ -1,12 +1,16 @@
 
+import { NavLink } from "react-router-dom"
 export const Header = ()=>{
     return(
         <div className="header-section">
             <h1>J3G</h1>
             <div className="header-navs">
-                <h2 id="navs">HOME</h2>
-                <h2 id="navs">DASHBOARD</h2>
-                <h2 id="navs">LOGIN</h2>
+                <NavLink id="navs" to="/">HOME</NavLink>
+                <NavLink id="navs" to="/Dashboard">DASHBOARD</NavLink>
+                <NavLink id="navs" to="/Login">LOGIN</NavLink>
+            </div>
+            <div>
+            <NavLink id="navs" to="/">SERVICES</NavLink>
             </div>
         </div>
     )
