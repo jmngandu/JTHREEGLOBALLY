a28db6e936b6b60bfb259433466fc1883f4f8b49
jmngandu <jamesmngandu@gmail.com>
2024-03-27 15:19:51 +0300

    commit

--- jthreeglobally/src/Components/Header.jsx
+++ jthreeglobally/src/Components/Header.jsx
@@ -1,9 +1,9 @@
 
-import { NavLink } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 export const Header = ()=>{
     return(
         <div className="header-section">
-            <h1>J3G</h1>
+            <Link to="/" id="logo">J3G</Link>
             <div className="header-navs">
                 <NavLink id="navs" to="/">HOME</NavLink>
                 <NavLink id="navs" to="/Dashboard">DASHBOARD</NavLink>
