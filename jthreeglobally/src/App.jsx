33575b79c56c0fd0ea647e85fbd0162113acf1d8
jmngandu <jamesmngandu@gmail.com>
2024-03-28 08:18:06 +0300

    created login page

--- jthreeglobally/src/App.jsx
+++ jthreeglobally/src/App.jsx
@@ -5,6 +5,7 @@ import { Dashboard } from './pages/Dashboard'
 import { Home } from './pages/Home'
 import { NotFound } from './Components/NotFound'
 import { Services } from './pages/Services'
+import { Login } from './pages/Login'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path="Dashboard" element={<Dashboard/>}/>
       <Route path='Services' element={<Services/>}/>
+      <Route path='Login' element={<Login/>}/>
       <Route path='*' element={<NotFound/>}/>
      </Routes>
     </>
---
+++ jthreeglobally/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+
+export const Login=()=>{
+    return(
+
+        <>
+        <h1>Login Here</h1>
+        </>
+    )
+}
