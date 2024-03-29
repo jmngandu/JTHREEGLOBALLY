50749df8b10a4785787c29544b8474df11e3c5c3
jmngandu <jamesmngandu@gmail.com>
2024-03-30 00:07:33 +0300

    commit

--- jthreeglobally/src/pages/Login.jsx
+++ jthreeglobally/src/pages/Login.jsx
@@ -6,6 +6,11 @@ export const Login=()=>{
         <div className="login-page">
         <h1>Login</h1>
         <small>welcome to jthreeglobally</small>
+        <div className="user-login">
+        <input type="text" placeholder="Username" id="user-input"/>
+        <input type="password" placeholder="Password" id="user-input" />
+
+        </div>
         </div>
         </>
     )
