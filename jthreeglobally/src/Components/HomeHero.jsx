c322995bd20e0e5e0aea7a64a9cdae005105701c
jmngandu <jamesmngandu@gmail.com>
2024-04-01 20:20:27 +0300

    commit

--- jthreeglobally/src/Components/HomeHero.jsx
+++ jthreeglobally/src/Components/HomeHero.jsx
@@ -5,15 +5,21 @@ export const HomeHero =()=>{
         <div className="home-heros">
             <div className="left-hero" id="hero-pages">
                 <h3>who are we?</h3>
-                <div className="pages-files"></div>
+                <div className="pages-files">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quibusdam facere dolor eveniet praesentium, debitis suscipit aspernatur adipisci nisi perspiciatis qui eaque reiciendis beatae excepturi ratione. Deleniti ea saepe rem.
+                </div>
             </div>
             <div className="center-hero" id="hero-pages">
                 <h3>what we do?</h3>
-                <div className="pages-files"></div>
+                <div className="pages-files">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat voluptate tempora accusamus laudantium tempore cupiditate deserunt. Dolore iste, reprehenderit eaque autem rerum veniam laudantium illum ipsum, debitis, ab animi excepturi.
+                </div>
             </div>
             <div className="right-hero" id="hero-pages">
             <h3>why choose us?</h3>
-            <div className="pages-files"></div>
+            <div className="pages-files">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet dolor rerum quae veritatis quos obcaecati tempore. Neque ad beatae laudantium suscipit asperiores molestiae nemo aut assumenda. Ipsum quasi dignissimos sed.
+            </div>
             </div>
         </div>
         </>
