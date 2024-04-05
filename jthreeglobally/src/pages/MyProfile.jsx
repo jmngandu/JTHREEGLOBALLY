eb32c19a25e249bc4019c5475a7822195fe3d69c
jmngandu <jamesmngandu@gmail.com>
2024-04-06 00:25:59 +0300

    created login page

--- jthreeglobally/src/pages/MyProfile.jsx
+++ jthreeglobally/src/pages/MyProfile.jsx
@@ -2,12 +2,14 @@ import { Header } from "../Components/Header"
 import { useState } from "react"
 
 export const MyProfile=()=>{
+    let unknownUser = 'user'
     let userName = "james"
-    const {welcome, setWelcome} = useState(`Welcome ${userName}`)
+    const [welcome, setWelcome] = useState(`Welcome ${userName}`)
     return(
         <>
         <Header/>
         <h1>{welcome}</h1>
+        <small onClick={()=>setWelcome(`Welcome ${unknownUser}`)}>Name</small>
         <h2>My profile</h2>
         </>
     )
