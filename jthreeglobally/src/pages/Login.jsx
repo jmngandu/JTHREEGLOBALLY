c9acce7fc0ef7224a4d5b17bb864d377a32b9912
jmngandu <jamesmngandu@gmail.com>
2024-04-05 23:12:47 +0300

    created login page

--- jthreeglobally/src/pages/Login.jsx
+++ jthreeglobally/src/pages/Login.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom"
 import { useState } from "react"
 export const Login=()=>{
+    const [message, setMessage] = useState('welcome to jthreeglobally')
     return(
 
         <>
         <div className="login-page">
         <h1>USER LOGIN</h1>
-        <small>welcome to jthreeglobally</small>
+        <small>{message}</small>
         <div className="user-login">
         <input type="text" placeholder="Username" id="user-input"/>
         <input type="password" placeholder="Password" id="user-input" />
         <button className="input-btn login-btn"><Link to="/Dashboard">LOGIN</Link></button>
-        <p><Link to="/Signup" id="links">forgotten password?</Link></p>
+        <p><Link to="/Login" id="links" onClick={()=>setMessage('check your email a message has been sent')}>forgotten password?</Link></p>
         <h3><Link to="/Signup" id="links">Signup?</Link></h3>
 
         </div>
