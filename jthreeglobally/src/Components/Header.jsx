53776a766e8eb65569219551da10fbffe492387a
jmngandu <jamesmngandu@gmail.com>
2024-04-06 00:14:53 +0300

    created login page

--- jthreeglobally/src/Components/Header.jsx
+++ jthreeglobally/src/Components/Header.jsx
@@ -7,7 +7,7 @@ export const Header = ()=>{
             <div className="header-navs">
                 <NavLink id="navs" to="/">HOME</NavLink>
                 <NavLink id="navs" to="/Dashboard">DASHBOARD</NavLink>
-                <NavLink id="navs" to="/Services">SERVICES</NavLink>
+                <NavLink id="navs" to="/Profile">PROFILE</NavLink>
             </div>
             <div>
             <NavLink id="navs" to="/Login">LOGIN</NavLink>
