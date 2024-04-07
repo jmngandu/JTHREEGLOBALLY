400439e067763c89fc7272f28c86e01dc58ec8a8
jmngandu <jamesmngandu@gmail.com>
2024-04-07 17:58:34 +0300

    commit

--- jthreeglobally/src/pages/MyProfile.jsx
+++ jthreeglobally/src/pages/MyProfile.jsx
@@ -10,7 +10,7 @@ export const MyProfile=()=>{
         <Header/>
         <h1>{welcome}</h1>
         <p>Name: <small>JAMES NGANDU</small></p>
-        <p>Username: <small>{userName}</small></p>
+        <p>username: <small>{userName}</small></p>
         <p>aboutme: <small>Am a computer scientist </small></p>
         <h3 onClick={()=>setWelcome(`Welcome`)}>logout</h3>
         
