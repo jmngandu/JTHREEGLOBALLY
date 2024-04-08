a7166e2320a2ab4f659ab3912f9fce4223a01feb
jmngandu <jamesmngandu@gmail.com>
2024-04-08 15:00:06 +0300

    commmit

--- jthreeglobally/src/pages/Dashboard.jsx
+++ jthreeglobally/src/pages/Dashboard.jsx
@@ -6,8 +6,36 @@ export const Dashboard = () =>{
         <Header/>
         <h1>OUR PRODUCTS</h1>
         <div className="products-section">
-            <div className="products-page"></div>
-            <div className="products-list"></div>
+            <div className="products-page">
+                <p>Category</p>
+                <h3>Fruits</h3>
+                <h3>Vegetables</h3>
+                <h3>Proteins</h3>
+                <h3>Dairy</h3>
+            </div>
+            <div className="products-list">
+                <div className="products">
+                <p>ksh 50</p>
+                <p>quantity</p>
+                <p>add to cart</p>
+                </div>
+                <div className="products">
+                <p>ksh 50</p>
+                <p>quantity</p>
+                <p>add to cart</p>
+                </div>
+                <div className="products">
+                <p>ksh 50</p>
+                <p>quantity</p>
+                <p>add to cart</p>
+                </div>
+                <div className="products">
+                <p>ksh 50</p>
+                <p>quantity</p>
+                <p>add to cart</p>
+                </div>
+                
+            </div>
         </div>
 
         </>
