f436662973e87890781331782e60fcaa69572bf7
jmngandu <jamesmngandu@gmail.com>
2024-04-08 17:10:10 +0300

    commmit

--- jthreeglobally/src/Components/ProductsItems.jsx
+++ jthreeglobally/src/Components/ProductsItems.jsx
@@ -4,8 +4,9 @@ export const ProductsItems=(myProducts)=>{
         <>
          <div className="products-list">
                 <div className="products">
-                <p>{myProducts.pro.capital}</p>
-                <p>{myProducts.pro.quantity}</p>
+                    <p>{myProducts.pro.name}</p>
+                <p>KSH: {myProducts.pro.capital}</p>
+                <p>Quantity: {myProducts.pro.quantity}</p>
                 <p>{myProducts.pro.purchase}</p>
                 </div>                
             </div>
