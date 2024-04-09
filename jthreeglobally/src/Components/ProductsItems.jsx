12ba3343a429cfeebabf3e1cf96424e2a1571bfb
jmngandu <jamesmngandu@gmail.com>
2024-04-09 08:59:40 +0300

    commit

--- jthreeglobally/src/Components/ProductsItems.jsx
+++ jthreeglobally/src/Components/ProductsItems.jsx
@@ -4,7 +4,7 @@ export const ProductsItems=(myProducts)=>{
         <>
          <div className="products-list">
                 <div className="products">
-                    <img src="/src/images/jlogo.png"/>
+                    <img src="/src/images/apple.png"/>
                     <p>{myProducts.pro.name}</p>
                 <p>KSH: {myProducts.pro.capital}</p>
                 <p>Quantity: {myProducts.pro.quantity}</p>
