0207f7eba25769d7d4a92aaf857aad1e8f3a6d47
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:20:10 +0300

    commit

--- jthreeglobally/src/pages/Home.jsx
+++ jthreeglobally/src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import { Header } from "../Components/Header"
-import { HomeHero } from "../Components/HomeHero"
-export const Home =()=>{
-    return(
-        <>
-        <Header/>
-        <div className="home-section">
-            <h1>WELCOME TO JTHREEGLOBALLY</h1>
-        </div>
-        <HomeHero/>
-        </>
-    )
-}
+import React from "react";
+import { Header } from "../Components/Header";
+import { HomeHero } from "../Components/HomeHero";
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <div className="home-section">
+        <h1>WELCOME TO JTHREEGLOBALLY</h1>
+      </div>
+      <HomeHero />
+    </>
+  );
+};
