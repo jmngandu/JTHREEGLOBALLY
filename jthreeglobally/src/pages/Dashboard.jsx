cd01ff3fbdcee5aad5ab4461ce552c55b0deb831
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:20:00 +0300

    commit

--- jthreeglobally/src/pages/Dashboard.jsx
+++ jthreeglobally/src/pages/Dashboard.jsx
@@ -1,16 +1,13 @@
-import { Header } from "../Components/Header"
-import { Products } from "../Components/Products"
+import { Header } from "../Components/Header";
+import { Products } from "../Components/Products";
 
-
-export const Dashboard = () =>{
-    return(
-        <>
-        <div className="header-dash">
-        <Header/>
-        </div>
-        <Products/>
-
-
-        </>
-    )
-}
+export const Dashboard = () => {
+  return (
+    <>
+      <div className="header-dash">
+        <Header />
+      </div>
+      <Products />
+    </>
+  );
+};
