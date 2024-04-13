0b5238a3551eab976ad5f9e46fbc1cd1e5aa9cdf
jmngandu <jamesmngandu@gmail.com>
2024-04-13 12:22:28 +0300

    commit

--- jthreeglobally/src/Components/ProductsItems.jsx
+++ jthreeglobally/src/Components/ProductsItems.jsx
@@ -1,16 +1,15 @@
-
-export const ProductsItems=(myProducts)=>{
-    return(
-        <>
-         <div className="products-list">
-                <div className="products">
-                    <img src="/src/images/apple.png"/>
-                    <p>{myProducts.pro.name}</p>
-                <p>KSH: {myProducts.pro.capital}</p>
-                <p>Quantity: {myProducts.pro.quantity}</p>
-                <p>{myProducts.pro.purchase}</p>
-                </div>                
-            </div>
-        </>
-    )
-}
+export const ProductsItems = (myProducts) => {
+  return (
+    <>
+      <div className="products-list">
+        <div className="products">
+          <img src="/src/images/apple.png" />
+          <p>{myProducts.pro.name}</p>
+          <p>KSH: {myProducts.pro.capital}</p>
+          <p>Quantity: {myProducts.pro.quantity}</p>
+          <p>{myProducts.pro.purchase}</p>
+        </div>
+      </div>
+    </>
+  );
+};
