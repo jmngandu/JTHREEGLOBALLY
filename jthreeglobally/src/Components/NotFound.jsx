9c530951c1e2498ef4c593b9e143324345040d22
jmngandu <jamesmngandu@gmail.com>
2024-04-23 19:57:24 +0300

    error page

--- jthreeglobally/src/Components/NotFound.jsx
+++ jthreeglobally/src/Components/NotFound.jsx
@@ -1,8 +1,9 @@
 export const NotFound = () => {
   return (
     <>
-      <h1>PAGE NOT FOUND</h1>
-      <h1>404</h1>
+      <h1>Oops, something went wrong</h1>
+      <small>But don't worry - it's not your fault</small>
+      {/* <h1>404</h1> */}
     </>
   );
 };
